01bb0681c82d92a9ddde2b32977fff701f04b7d3
VitorPontes <vitoripontes2018@gmail.com>
2025-03-20 18:06:00 -0300

    new file:   public/images.jpg 	new file:   public/img/churras png.jpg 	new file:   public/img/churrasco.png 	renamed:    img/hero.png -> public/img/hero.png 	new file:   "public/img/macarr\303\243o.png" 	new file:   public/img/ramen.png 	new file:   public/img/tigela_lamen.avif 	new file:   public/sla.png 	modified:   src/App.vue 	new file:   src/scss/style.scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <header>
-   <nav class="navbar">
+  <div class="container">
+    <nav class="navbar">
         <i class="fa-solid fa-burger navbar__logo">FOOD</i>
 
         <ul id="nav_list">
@@ -23,6 +24,9 @@
           <i class="fa-solid fa-bars"></i>
         </button>
    </nav>
+
+  </div>
+   
    <div class="mobile_menu" :class="{ active: isMenuActive }">
 
         <ul class="mobile_nav">
@@ -44,17 +48,18 @@
    </header>
 
    <main id="content"> 
-      <section class="home">
-        <div class="shape"></div>
+      <section class="hero">
+        <div class="container">
+          <div class="shape"></div>
         <div id="cta">
           <h1 class="title">
             O Sabor vai até 
             <span>você</span>
+
           </h1>
 
           <P class="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam 
-            veritatis ipsum error necessitatibus optio, incidunt dignissimos earum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           </P>
 
 
@@ -66,12 +71,12 @@
             <a href="tel:+40028922" id="phone_button">
               <Button class="btn-default">
                 <i class="fa-solid fa-phone"></i>
-                (11) 40028922
               </Button>
+              (11) 40028922
             </a>
           </div>
 
-          <div class="social-media-buttons">
+          <div class="social-buttons">
             <a href="">
               <i class="fa-brands fa-whatsapp"></i>
             </a>
@@ -85,10 +90,150 @@
             </a>
           </div>
         </div>
+
+        <div class="banner">
+            <img src="img/hero.png" alt="">
+        </div>
+
+        </div>
+        
       </section>
-      <div id="banner">
-      <img src="hero.png" alt="#">
-    </div>phg
+
+      <section id="sesc">
+        <h2 class="sesc-title">Cardápior</h2>
+        <div class="Sesc-sub">Nossos pratos especiais</div>
+
+        <div class="pratos">
+
+          <div class="dish">
+            <div class="dish_heart">
+              <i class="fa-solid fa-heart"></i>
+            </div>
+
+            <img src="/ramen.png" class="dish-heart" alt="">
+
+            <h3 class="dish-title">
+              Ramen de misso/shoyu
+            </h3>
+
+            <span class="dish-description">
+              Deliciosa sopa japonesa com caldo rico e aromático, macarrão artesanal, fatias suculentas de carne
+            </span>
+
+            <div class="dish-rate">
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <span>(500+)</span>
+            </div>
+
+            <div class="dish-price">
+              <h4 class="">R$40,00</h4>
+              <button class="btn-default">
+                <i class="fa-solid fa-basket-shopping"></i>
+              </button>
+            </div>
+          </div>
+
+          <div class="dish">
+            <div class="dish_heart">
+              <i class="fa-solid fa-heart"></i>
+            </div>
+
+            <img src="img/macarrão.png" class="dish-heart" alt="">
+
+            <h3 class="dish-title">
+              Macarrão a moda Italiana
+            </h3>
+
+            <span class="dish-description">
+              O macarrão um prato tradicional da culinária italiana, feito com massa cozida e acompanhado por diversos molhos.
+            </span>
+
+            <div class="dish-rate">
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <span>(350+)</span>
+            </div>
+
+            <div class="dish-price">
+              <h4 class="">R$25,00</h4>
+              <button class="btn-default">
+                <i class="fa-solid fa-basket-shopping"></i>
+              </button>
+            </div>
+          </div>
+
+          <div class="dish">
+            <div class="dish_heart">
+              <i class="fa-solid fa-heart"></i>
+            </div>
+
+            <img src="img/churrasco.png" class="dish-heart" alt="">
+
+            <h3 class="dish-title">
+              Prato de Churrasco
+            </h3>
+
+            <span class="dish-description">
+              O churrasco é um prato tradicional que consiste em carnes assadas na grelha ou espeto
+            </span>
+
+            <div class="dish-rate">
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <span>(400+)</span>
+            </div>
+
+            <div class="dish-price">
+              <h4 class="">R$50,00</h4>
+              <button class="btn-default">
+                <i class="fa-solid fa-basket-shopping"></i>
+              </button>
+            </div>
+          </div>
+
+          <div class="dish">
+            <div class="dish_heart">
+              <i class="fa-solid fa-heart"></i>
+            </div>
+
+            <img src="images.jpg" class="dish-heart" alt="">
+
+            <h3 class="dish-title">
+              Esrogonofe de Carne
+            </h3>
+
+            <span class="dish-description">
+              O estrogonofe de carne é um prato de origem russa que se tornou muito popular no Brasil. 
+            </span>
+
+            <div class="dish-rate">
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <div class="fa-solid fa-star"></div>
+              <span>(550+)</span>
+            </div>
+
+            <div class="dish-price">
+              <h4 class="">R$40,00</h4>
+              <button class="btn-default">
+                <i class="fa-solid fa-basket-shopping"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
    </main>
 
 </template>
@@ -109,275 +254,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
-.poppins-thin {
-  font-family: "Poppins", sans-serif;
-  font-weight: 100;
-  font-style: normal;
-}
-
-.poppins-extralight {
-  font-family: "Poppins", sans-serif;
-  font-weight: 200;
-  font-style: normal;
-}
-
-.poppins-light {
-  font-family: "Poppins", sans-serif;
-  font-weight: 300;
-  font-style: normal;
-}
-
-.poppins-regular {
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  font-style: normal;
-}
-
-.poppins-medium {
-  font-family: "Poppins", sans-serif;
-  font-weight: 500;
-  font-style: normal;
-}
-
-.poppins-semibold {
-  font-family: "Poppins", sans-serif;
-  font-weight: 600;
-  font-style: normal;
-}
-
-.poppins-bold {
-  font-family: "Poppins", sans-serif;
-  font-weight: 700;
-  font-style: normal;
-}
-
-.poppins-extrabold {
-  font-family: "Poppins", sans-serif;
-  font-weight: 800;
-  font-style: normal;
-}
-
-.poppins-black {
-  font-family: "Poppins", sans-serif;
-  font-weight: 900;
-  font-style: normal;
-}
-
-.poppins-thin-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 100;
-  font-style: italic;
-}
-
-.poppins-extralight-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 200;
-  font-style: italic;
-}
-
-.poppins-light-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 300;
-  font-style: italic;
-}
-
-.poppins-regular-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  font-style: italic;
-}
-
-.poppins-medium-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 500;
-  font-style: italic;
-}
-
-.poppins-semibold-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 600;
-  font-style: italic;
-}
-
-.poppins-bold-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 700;
-  font-style: italic;
-}
-
-.poppins-extrabold-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 800;
-  font-style: italic;
-}
-
-.poppins-black-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 900;
-  font-style: italic;
-}
-
-* {
-    font-family: "Poppins", sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-body{
-  background-color: rgb(252, 244, 232);
-}
-
-section{
-    padding: 28px 6%;
-
-}
-
-header {
-  width: 100%;
-  padding: 20px 8%;
-}
-
-.navbar {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    &__logo{
-  font-size: 24px;
-  color: darkorange;
-}
-}
-
-#nav_list {
-  display: flex;
-  list-style: none;
-  gap: 48px;
-}
-
-#mobile_btn {
-  display: none;
-}
-
-.mobile_menu {
-  display: none;
-
-  .nav_item_mobile {
-  text-decoration: none;
-  color: #1d1d1dad;
-  font-weight: 600;
-
-  &:hover{
-    color:#f1dd45;
-  }
-}
-
-}
+<style src="./scss/style.scss" lang="scss"/>
+ 
 
 
-
-.nav-item a{
-  text-decoration: none;
-  color: #1d1d1dad;
-  font-weight: 600;
-  position: relative;
-  
-  &::before{
-    content: '';
-    width: 0;
-    height: 2px;
-    position: absolute;
-    bottom: -2px;
-    left: 0;
-    transition: .5s ease-in-out;
-    background-color: #fac53e;
-  }
-
-  &:hover{
-
-  &::before{
-    width: 100%;
-
-  }
-  }
-}
-
-.nav-item.active a{
-    color: black;
-    border-bottom: 3px solid #f1f0e1;
-    
-}
-
-.nav_item_mobile a{
-  text-decoration: none;
-  color: #1d1d1dad;
-  font-weight: 600;
-}
-
-
-
-.btn-default {
-  border: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #fac53e;
-  border-radius: 12px;
-  padding: 10px 14px;
-  font-weight: 600;
-  box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-  transition: background-color .3s ease;
-}
-
-.btn-default:hover{
-  background-color: #f7ddad;
-}
-
-@media screen and (max-width: 1170px){
-    #nav_list,
-    .navbar .btn-default {
-      display:none;
-    }
-
-    #mobile_btn{
-      display:block;
-      border: none;
-      background-color: transparent;
-      font-size: 1.5rem;
-      cursor: pointer;
-    }
-
-    .mobile_menu.active {
-      display:flex;
-      flex-direction: column;
-      align-items: center;
-    }
-
-    .mobile_nav{
-      display: flex;
-      flex-direction: column;
-      gap: 12px;
-      margin: 12px 0px; 
-      list-style: none;
-      text-align: center;
-
-    .nav_item_mobile {
-      list-style-type: none;
-      color: gray;
-    }
-    }
-   
-  }
-
-
-  #cta {
-  display: flex;
-  min-height: 100vh - 75;
-  
-  }
-  
-</style>
